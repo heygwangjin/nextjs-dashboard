b72c5a9743c0c772242ccb63a48b8ad08b2803e1
heygwangjin <heygwangjin@gmail.com>
2024-07-19 03:37:04 +0900

    feat: Add dashboard

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,6 +1,9 @@
 import { LatestInvoice } from '@/app/lib/definitions';
 import { lusitana } from '@/app/lib/fonts';
+import { cn } from '@/app/lib/utils';
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
+
 export default async function LatestInvoices({
   latestInvoices,
 }: {
@@ -12,9 +15,7 @@ export default async function LatestInvoices({
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: Uncomment this code in Chapter 7 */}
-
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -51,7 +52,7 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500">Updated just now</h3>
