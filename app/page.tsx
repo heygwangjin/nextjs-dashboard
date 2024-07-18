24fce848645bf785f549dc9b22dc1a527c47900b
heygwangjin <heygwangjin@gmail.com>
2024-07-18 16:49:03 +0900

    feat: Add hero image

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 import Link from 'next/link';
 import AcmeLogo from './ui/acme-logo';
 
@@ -26,7 +27,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000} // width, height를 원본 이미지와 동일한 비율로 설정해야 layout shift가 발생하지 않는다.
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
