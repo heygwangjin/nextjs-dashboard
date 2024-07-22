8a936d7c64f74e1752095ed71cbebb9804ff6c9e
heygwangjin <heygwangjin@gmail.com>
2024-07-22 17:43:14 +0900

    feat: Add logout

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -72,7 +72,7 @@ export default function LoginForm() {
           {errorMessage && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-50">{errorMessage}</p>
+              <p className="text-sm text-red-500">{errorMessage}</p>
             </>
           )}
         </div>
